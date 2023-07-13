f0013db40de7445211249dd0758a4136ba5ec9f3
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-07-13 16:30:38 +0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,3 +37,25 @@ comment = updated_post.comments.find_by(text: 'Hi Tom!') if updated_post
 puts "Comment Text: #{comment.text}" if comment
 comment.destroy if comment
 puts "Comment Count after deletion: #{updated_post.comments.count}" if updated_post
+
+# Call the methods
+recent_posts = user.recent_posts
+puts "Recent Posts for User: #{user.name}"
+recent_posts.each do |post|
+  puts "Title: #{post.title}, Text: #{post.text}"
+end
+
+updated_user.update_posts_counter
+puts "Posts Counter for User: #{updated_user.name} - #{updated_user.posts_counter}"
+
+recent_comments = updated_post.recent_comments
+puts "Recent Comments for Post: #{updated_post.title}"
+recent_comments.each do |comment|
+  puts "Text: #{comment.text}"
+end
+
+updated_post.update_comments_counter
+puts "Comments Counter for Post: #{updated_post.title} - #{updated_post.comments_counter}"
+
+updated_post.update_likes_counter
+puts "Likes Counter for Post: #{updated_post.title} - #{updated_post.likes_counter}"
