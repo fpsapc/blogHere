ac263ceccb1042b639045225067d74228cc33a41
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-07-14 20:36:29 +0500

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :likes
 
   after_create :update_author_posts_counter
-  
+
   validates :title, presence: true
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe 'validations for Post model' do
     before(:each) do
-      @user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
-      @post = Post.new(author: @user, title: 'Hello', text: 'This is my first post', likes_counter: 0, comments_counter: 0)
+      @user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.',
+                          posts_counter: 0)
+      @post = Post.new(author: @user, title: 'Hello', text: 'This is my first post', likes_counter: 0,
+                       comments_counter: 0)
     end
 
     it 'Title must not be blank' do
