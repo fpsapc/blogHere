67fcdd14d9c07141775eb1cd2fb5cedcfe657663
Daniela Moreno <dani9292@hotmail.com>
2023-07-24 15:24:15 -0500

    displays first comments for a post

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -31,28 +31,39 @@ RSpec.describe "Post Index", type: :system do
       expect(page).to have_content(user.posts_counter)
     end
 
-#   it "displays a post title on user posts page" do
-#     post = Post.create(Title: "My First Post", Text: "This is my first post", author_id: user.id, CommentsCounter: 0, LikesCounter: 0)
-#     visit user_posts_path(user_id: user.id)
-      
-#       expect(page).to have_content(post.Title)
-#     end
+  it "displays a post title on user posts page" do
+    user = User.create(name: "Tom Jenkins", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqI5QUajL66Kkwz9efY-xCyC_Bq97XyTvgPmndMocETt62-ps3VjrzLtWNrDq_Y5qa5fQ", posts_counter: 5)
 
-#   it "displays a post text on user posts page" do
-#     post = Post.create(Title: "My First Post", Text: "This is my first post", author_id: user.id, CommentsCounter: 0, LikesCounter: 0)
-#     visit user_posts_path(user_id: user.id)
+    post = Post.create(title: "My First Post", text: "This is my first post", author_id: user.id, comments_counter: 0, likes_counter: 0)
+    
+    
+    visit user_posts_path(user_id: user.id)
       
-#       expect(page).to have_content(post.Text)
-#     end
+      expect(page).to have_content(post.title)
+    end
+
+  it "displays a post text on user posts page" do
+    user = User.create(name: "Tom Jenkins", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqI5QUajL66Kkwz9efY-xCyC_Bq97XyTvgPmndMocETt62-ps3VjrzLtWNrDq_Y5qa5fQ", posts_counter: 5)
+
+    post = Post.create(title: "My First Post", text: "This is my first post", author_id: user.id, comments_counter: 0, likes_counter: 0)
+    
+    visit user_posts_path(user_id: user.id)
+      
+      expect(page).to have_content(post.text)
+    end
 
   
-#   it "displays the first comments on a post" do
-#     post = Post.create(Title: "My First Post", Text: "This is my first post", author_id: user.id, CommentsCounter: 0, LikesCounter: 0)
-#     comment = Comment.create(Text: "This is my first comment", author_id: user.id, post_id: post.id)
-#     visit user_posts_path(user_id: user.id)
-      
-#       expect(page).to have_content(comment.Text)
-#     end
+  it "displays the first comments on a post" do
+  
+    user = User.create(name: "Tom Jenkins", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqI5QUajL66Kkwz9efY-xCyC_Bq97XyTvgPmndMocETt62-ps3VjrzLtWNrDq_Y5qa5fQ", posts_counter: 5)
+
+    post = Post.create(title: "My First Post", text: "This is my first post", author_id: user.id, comments_counter: 0, likes_counter: 0)
+    comment = Comment.create(text: "This is my first comment", author_id: user.id, post_id: post.id)
+    visit user_posts_path(user_id: user.id)
+    
+    
+      expect(page).to have_content(comment.text)
+    end
 
 #     it "displays the number of comments on a post" do
 #       post = Post.create(Title: "My First Post", Text: "This is my first post", author_id: user.id, CommentsCounter: 0, LikesCounter: 0)
