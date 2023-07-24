a07c29eec7497e3e35be7f21b3f78fb3c75a1247
Daniela Moreno <dani9292@hotmail.com>
2023-07-24 15:41:41 -0500

    display users comment text

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -44,10 +44,14 @@ RSpec.describe "Post Show", type: :system do
     expect(page).to have_content(comment.author.name)
   end
 
-#   it "displays the comment text" do
-#     visit user_post_path(user_id: user.id, id: post.id)
-#     expect(page).to have_content(comment.Text)
-#   end
+  it "displays the comment text" do
+    user = User.create(name: "Tom Jenkins", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqI5QUajL66Kkwz9efY-xCyC_Bq97XyTvgPmndMocETt62-ps3VjrzLtWNrDq_Y5qa5fQ", posts_counter: 5)
+    post = Post.create(title: "My First Post", text: "This is my first post", author_id: user.id, comments_counter: 0, likes_counter: 0)
+ 
+  comment = Comment.create(text: "This is my first comment", author_id: user.id, post_id: post.id)
+    visit user_post_path(user_id: user.id, id: post.id)
+    expect(page).to have_content(comment.text)
+  end
 
 
 end
