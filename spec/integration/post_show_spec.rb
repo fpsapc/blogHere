da6b341189b042a76ea22b02ebd1fea700016425
Daniela Moreno <dani9292@hotmail.com>
2023-07-24 15:40:18 -0500

    display users commentor

--- spec/integration/post_show_spec.rb
+++ spec/integration/post_show_spec.rb
@@ -25,20 +25,24 @@ RSpec.describe "Post Show", type: :system do
     expect(page).to have_content(post.comments_counter)
   end
 
-#   it "displays how many likes the post has" do
-#     visit user_post_path(user_id: user.id, id: post.id)
-#     expect(page).to have_content(post.LikesCounter)
-#   end
+  it "displays how many likes the post has" do
+    visit user_post_path(user_id: user.id, id: post.id)
+    expect(page).to have_content(post.likes_counter)
+  end
 
-#   it "displays the post text" do
-#     visit user_post_path(user_id: user.id, id: post.id)
-#     expect(page).to have_content(post.Text)
-#   end
+  it "displays the post text" do
+    visit user_post_path(user_id: user.id, id: post.id)
+    expect(page).to have_content(post.text)
+  end
 
-#   it "displays the user name commentor" do
-#     visit user_post_path(user_id: user.id, id: post.id)
-#     expect(page).to have_content(comment.user.name)
-#   end
+  it "displays the user name commentor" do
+    user = User.create(name: "Tom Jenkins", photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqI5QUajL66Kkwz9efY-xCyC_Bq97XyTvgPmndMocETt62-ps3VjrzLtWNrDq_Y5qa5fQ", posts_counter: 5)
+    post = Post.create(title: "My First Post", text: "This is my first post", author_id: user.id, comments_counter: 0, likes_counter: 0)
+ 
+  comment = Comment.create(text: "This is my first comment", author_id: user.id, post_id: post.id)
+    visit user_post_path(user_id: user.id, id: post.id)
+    expect(page).to have_content(comment.author.name)
+  end
 
 #   it "displays the comment text" do
 #     visit user_post_path(user_id: user.id, id: post.id)
